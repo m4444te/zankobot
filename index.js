aae21dfeec7906bd47f5d0a0b21990df790a649e
Mathieu Mescam <mate@e.email>
2024-09-11 09:26:15 +0200

    update log function to Paris time

--- index.js
+++ index.js
@@ -18,9 +18,20 @@ let imageFiles = [];
 let currentIndex = 0;
 
 function log(message, level = 'info') {
-  const timestamp = new Date().toISOString();
+  const timestamp = new Intl.DateTimeFormat('fr-FR', {
+    timeZone: 'Europe/Paris',
+    hour12: false,
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  }).format(new Date());
+
   console[level](`[${timestamp}] [${level.toUpperCase()}] ${message}`);
 }
+
 
 async function ensureDirectoryExists(directory) {
   try {
